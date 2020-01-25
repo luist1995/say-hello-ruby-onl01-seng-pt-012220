729b0c5ec0f1b38581ebb4a554cc1eb874e92766
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-01-25 01:58:49 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,4 @@
 # Build your say_hello method here
-def say_hello(name)
-  name = "Ruby Programmer"
-  puts "hello Gabriela!"
+def say_hello(name = "Ruby Programmer")
+  puts " Hello #{name}!"
 end
-
